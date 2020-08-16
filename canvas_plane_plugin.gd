123b8e191da2674e95579a537884ed7780ab41d2
Saracen <SaracenOne@gmail.com>
2020-08-16 22:13:23 +0100

    Add printed messages for initialisation and destruction.

--- canvas_plane_plugin.gd
+++ canvas_plane_plugin.gd
@@ -3,6 +3,15 @@ tool
 
 var editor_interface = null
 
+func _init():
+	print("Initialising CanvasPlane plugin")
+
+
+func _notification(p_notification: int):
+	match p_notification:
+		NOTIFICATION_PREDELETE:
+			print("Destroying CanvasPlane plugin")
+
 
 func get_name():
 	return "CanvasPlane"
